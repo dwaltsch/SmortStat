a69da5988ce257628b9ad96eea508e25c2e7769e
Michael <39913724+dwaltsch@users.noreply.github.com>
2020-12-26 20:49:57 +0100

    changed Port

--- server.py
+++ server.py
@@ -91,4 +91,4 @@ if __name__== "__main__":
     print("ignore the errors")
     print("Enter this in the APP")
     print(ip)
-    app.run(host ='0.0.0.0', port=5000,)
+    app.run(host ='0.0.0.0', port=80,)
