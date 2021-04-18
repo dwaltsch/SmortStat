2e6e961c9e6f1f510f34093a928ab188f0c72608
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2021-04-18 19:32:23 +0200

    Teil 2

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
 # SmortStat
 
 Source Code of the SmortStat Client an App to Monitor your PC remotly
-
-# Installation
-
-* From the Website:
-
- 1. Download from the Website
- 2. unzip the file
- 3. execute as Admin
-
-* From Github:
- 1. go to the releases tab
- 2. download and unzip
- 3. execute as Admin
  
 # Authors
 * [Dwaltsch](https://github.com/dwaltsch)
@@ -27,8 +14,10 @@ If you encounter any problems make sure you
 If this doesnt help make sure to open an Issue
 
 # Roadmap
-Beta Release at the End of 2020
-Season Patch every 2-3 months with new content
+Release sometime in the next 5 years
+
+#Branches
+Download from dev branch if you encounter any problems
 
 # Contributing
 Pull requests are welcome. For future features open an Issue or make a Pull Request
--- server.py
+++ server.py
@@ -14,14 +14,11 @@ import logging
 import platform
 
 #vars
-ip = socket.gethostbyname(socket.gethostname())
+ip = socket.gethostbyname(socket.gethostname()) # fix wrong IP
 app = Flask(__name__)
 
-OHM_hwtypes = [ 'Mainboard', 'SuperIO', 'CPU', 'RAM', 'GpuNvidia', 'GpuAti', 'TBalancer', 'Heatmaster', 'HDD' ]
-OHM_sensortypes = [
- 'Voltage', 'Clock', 'Temperature', 'Load', 'Fan', 'Flow', 'Control', 'Level', 'Factor', 'Power', 'Data', 'SmallData'
-]
-entsch = "no"
+#currently based on flask and psutil rewrite in c++
+
 
 @app.route('/')
 def home():
@@ -35,6 +32,7 @@ def CPU():
                    CPUCORECOUNT=str(psutil.cpu_count(logical=True)),
                    CPUSPEED=str(psutil.cpu_freq(percpu=True)),
                    )
+
 @app.route('/LAN', methods=['GET'])
 def LAN():
     app.logger.error('LAN Request')
@@ -48,40 +46,9 @@ def Name():
 
 @app.route('/TEMP', methods=['GET'])
 def TEMP():
-    app.logger.error('Temp Request')
-    def init_OHM():
-        clr.AddReference(os.path.abspath(os.path.dirname(__file__)) + R'\OpenHardwareMonitorLib.dll')
-        from OpenHardwareMonitor import Hardware
-        hw = Hardware.Computer()
-        hw.CPUEnabled, hw.GPUEnabled, = True, True,
-        hw.Open()
-        return hw
+    return "not yet implemented" #implement some way or switch plattform
 
-    def fetch_data(handle):
-        out = []
-        for i in handle.Hardware:
-            i.Update()
-            for sensor in i.Sensors:
-                thing = parse_sensor(sensor)
-                if thing is not None:
-                    out.append(thing)
-            for j in i.SubHardware:
-                j.Update()
-                for subsensor in j.Sensors:
-                    thing = parse_sensor(subsensor)
-                    out.append(thing)
-        return out
 
-    def parse_sensor(snsr):
-        if snsr.Value is not None:
-            if snsr.SensorType == OHM_sensortypes.index('Temperature'):
-                HwType = OHM_hwtypes[snsr.Hardware.HardwareType]
-                return {"Reading": snsr.Value}
-
-    def main():
-        return json.dumps({platform.node(): fetch_data(init_OHM())}, indent=1, sort_keys=True, ensure_ascii=False)
-
-    return main()
 @app.route('/RAM', methods=['GET'])
 def RAM():
     app.logger.error('RAM Request')
@@ -99,22 +66,19 @@ def sdw():
     os.system("shutdown /s /t 1");
     return "Shutting Down"
 
-@app.route('/UBD', methods=['GET'])
+@app.route('/UBD', methods=['GET']) #not implemented
 def update():
-    bst = platform.system()
-    if bst == "Windows":
+    os = platform.system()
+    if os == "Windows":
         print("Warning: Pog")
         return "Windows detected: Updating via Command Line"
-    if bst == "Linux":
+    if os == "Linux":
         return "Linux detected: Updating via Command Line"
-    if bst == "Darwin":
+    if os == "Darwin":
         return "MacOS is not officially supported yet"
 
 
 if __name__== "__main__":
-    print("enable logs yes or no")
-    entsch = input()
-    print("ignore the errors")
     print("Enter this in the APP")
     print(ip)
     app.run(host ='0.0.0.0', port=80,)
